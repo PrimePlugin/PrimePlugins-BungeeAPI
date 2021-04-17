492fb6f62b4c821edabfc973d090a941c0a58d00
Lukas <48287550+PrimeAPI@users.noreply.github.com>
2021-04-18 01:31:44 +0200

    Addded exemption handler to DatabaseTask; DatabaseTask runs async

--- src/main/java/de/primeapi/primeplugins/bungeeapi/sql/DatabaseTask.java
+++ src/main/java/de/primeapi/primeplugins/bungeeapi/sql/DatabaseTask.java
@@ -3,6 +3,7 @@ package de.primeapi.primeplugins.bungeeapi.sql;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.sql.PreparedStatement;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +16,13 @@ public class DatabaseTask<T> {
     private CompletableFuture<T> future;
 
     public void submit(Consumer<T> consumer){
-        future.thenAccept(consumer);
+        future.handle((unused, throwable) -> {
+            if(throwable != null) {
+                throwable.printStackTrace();
+                return null;
+            }
+            return unused;
+        }).thenAcceptAsync(consumer);
     }
 
 
