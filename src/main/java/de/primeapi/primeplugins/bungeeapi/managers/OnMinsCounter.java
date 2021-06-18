d26fab86933f3f1b2934e13034f704d2525bf83d
Lukas <48287550+PrimeAPI@users.noreply.github.com>
2021-06-19 00:43:18 +0200

    Added BungeeAPI. Stopped playtime adding when afk

--- src/main/java/de/primeapi/primeplugins/bungeeapi/managers/OnMinsCounter.java
+++ src/main/java/de/primeapi/primeplugins/bungeeapi/managers/OnMinsCounter.java
@@ -1,6 +1,8 @@
 package de.primeapi.primeplugins.bungeeapi.managers;
 
+import de.primeapi.primeplugins.bungeeapi.api.BungeeAPI;
 import de.primeapi.primeplugins.bungeeapi.api.PrimePlayer;
+import de.primeapi.primeplugins.bungeeapi.sql.utils.OnlineStats;
 import net.md_5.bungee.api.ProxyServer;
 
 public class OnMinsCounter implements Runnable {
@@ -9,7 +11,15 @@ public class OnMinsCounter implements Runnable {
     public void run() {
         ProxyServer.getInstance().getPlayers().forEach(proxiedPlayer -> {
             PrimePlayer p = new PrimePlayer(proxiedPlayer);
-            p.retrieveOnMins().submit(integer -> p.setOnMins(integer + 1));
+            if(BungeeAPI.getInstance().isOnline()) {
+                OnlineStats.getAFK(p.getUniqueId()).submit(aBoolean -> {
+                    if (!aBoolean) {
+                        p.retrieveOnMins().submit(integer -> p.setOnMins(integer + 1));
+                    }
+                });
+            }else {
+                p.retrieveOnMins().submit(integer -> p.setOnMins(integer + 1));
+            }
         });
     }
 }
---
+++ src/main/java/de/primeapi/primeplugins/bungeeapi/sql/utils/OnlineStats.java
@@ -0,0 +1,79 @@
+package de.primeapi.primeplugins.bungeeapi.sql.utils;
+
+import de.primeapi.primeplugins.bungeeapi.PrimeCore;
+import de.primeapi.primeplugins.bungeeapi.sql.DatabaseTask;
+import de.primeapi.primeplugins.bungeeapi.sql.party.PlayerParty;
+import lombok.NonNull;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class OnlineStats {
+
+    public static void flush() {
+        PrimeCore.getInstance().getDatabase().update("DELETE FROM `prime_bungee_online`").execute();
+    }
+
+    public static void insertPlayer(@NonNull UUID uuid) {
+        PrimeCore.getInstance().getDatabase().update("INSERT INTO `prime_bungee_online` VALUES (?,?,?,?)")
+                .parameters(uuid.toString(), null, 0, null)
+                .execute();
+    }
+
+    public static void removePlayer(@NonNull UUID uuid) {
+        PrimeCore.getInstance().getDatabase().update("DELETE FROM prime_bungee_online WHERE uuid = ?")
+                .parameters(uuid.toString())
+                .execute();
+    }
+
+    public static void setServer(@NonNull UUID uuid, String s) {
+        PrimeCore.getInstance().getDatabase().update("UPDATE prime_bungee_online SET server = ? WHERE uuid = ?")
+                .parameters(s, uuid.toString())
+                .execute();
+    }
+
+    public static void setAFK(@NonNull UUID uuid, boolean b) {
+        PrimeCore.getInstance().getDatabase().update("UPDATE prime_bungee_online SET afk = ? WHERE uuid = ?")
+                .parameters((b ? 1 : 0), uuid.toString())
+                .execute();
+    }
+
+    public static void setParty(@NonNull UUID uuid, UUID party) {
+        if (party == null) {
+            PrimeCore.getInstance().getDatabase().update("UPDATE prime_bungee_online SET party = null WHERE uuid = ?")
+                    .parameters(uuid.toString())
+                    .execute();
+        } else {
+            PrimeCore.getInstance().getDatabase().update("UPDATE prime_bungee_online SET party = ? WHERE uuid = ?")
+                    .parameters(party.toString(), uuid.toString())
+                    .execute();
+        }
+    }
+
+    public static DatabaseTask<String> getServer(@NonNull UUID uuid) {
+        return new DatabaseTask<>(CompletableFuture.supplyAsync(() -> PrimeCore.getInstance().getDatabase().select("SELECT server FROM prime_bungee_online WHERE uuid = ?")
+                .parameters(uuid.toString())
+                .getAs(String.class)
+                .toBlocking().singleOrDefault(null)));
+    }
+
+    public static DatabaseTask<Boolean> getAFK(@NonNull UUID uuid) {
+        return new DatabaseTask<>(CompletableFuture.supplyAsync(() -> PrimeCore.getInstance().getDatabase().select("SELECT afk FROM prime_bungee_online WHERE uuid = ?")
+                .parameters(uuid.toString())
+                .getAs(Integer.class)
+                .toBlocking().singleOrDefault(null) == 1));
+    }
+
+    public static DatabaseTask<PlayerParty> getParty(@NonNull UUID uuid) {
+        return new DatabaseTask<>(CompletableFuture.supplyAsync(() -> {
+            String s = PrimeCore.getInstance().getDatabase().select("SELECT party FROM prime_bungee_online WHERE uuid = ?")
+                    .parameters(uuid.toString())
+                    .getAs(String.class)
+                    .toBlocking().singleOrDefault(null);
+            if (s == null) return null;
+            return new PlayerParty(UUID.fromString(s));
+        }));
+    }
+
+
+}
